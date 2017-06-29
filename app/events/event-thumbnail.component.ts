a355299cfd4eeb8aec70b1b5e787f7df52ee5c73
Chappie72 <davetheunissen@gmail.com>
2017-06-29 19:58:11 +1200

    Exploring Angular's CSS encapsulation

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,15 +1,14 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'event-thumbnail',
-    templateUrl: 'app/events/event-thumbnail.component.html'
+    templateUrl: 'app/events/event-thumbnail.component.html',
+    styles:[`
+    .pad-left { margin-left: 10px; }
+    .well div { color: red; }
+    `]
 })
 
 export class EventThumbnailComponent {
     @Input() event:any
-    someProperty:any = "some value"
-
-    logFoo() {
-        console.log('foo')
-    }
 }
