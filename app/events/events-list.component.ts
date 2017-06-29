019488a49947ddc1eccfee21ea2aeab8143ad1af
Chappie72 <davetheunissen@gmail.com>
2017-06-29 19:31:25 +1200

    Communicating with child components using @Input events.

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,27 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'events-list',
-    template: `
-    <div>
-        <h1>Upcoming Angular2 Events</h1>
-        <hr/>
-        <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>{{event.date}}</div>
-            <div>{{event.time}}</div>
-            <div>\${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span>&nbsp;</span>
-                <span>{{event.location.city}}, {{event.location.country}}</span>
-            </div>
-        </div>
-    </div>
-    `
+    templateUrl: 'app/events/events-list.component.html' 
 })
 
 export class EventListComponent{
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '06/07/2017',
