235898007106b92f053b61e77ec8aa2ff70b6177
Chappie72 <davetheunissen@gmail.com>
2017-06-30 21:39:44 +1200

    property binding with ngSwitch, ngClass & ngStyle

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,6 +4,8 @@ import { Component, Input } from '@angular/core';
     selector: 'event-thumbnail',
     templateUrl: 'app/events/event-thumbnail.component.html',
     styles:[`
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
     .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
@@ -12,4 +14,9 @@ import { Component, Input } from '@angular/core';
 
 export class EventThumbnailComponent {
     @Input() event:any
+
+    getStartTimeClass(){
+        const isEarlyStart = this.event && this.event.time === '8:00 am'
+        return {green: isEarlyStart, bold: isEarlyStart}
+    }
 }
